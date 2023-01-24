868dfc5d4490e55752cdb4948233ac0b2fffdb73
theobcl <tbancal@free.fr>
2023-01-24 21:51:39 +0100

    Add index/create shop methods + shops seed

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,16 @@
 class ShopsController < ApplicationController
   def index
+    @shops = Shop.all
+  end
+
+  def create
+    @shop = shop.new(shop_params)
+    @shop.save
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name)
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,3 @@
 class Shop < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
