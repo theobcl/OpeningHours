9f9c12dc86e9432d158b11d871a711840f95bfb8
theobcl <tbancal@free.fr>
2023-01-25 20:23:02 +0100

    Homepage front done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
 require 'faker'
 
+if Shop.any?
+  puts "Destroy existing shops"
+  Shop.destroy_all
+  puts "Shops destruction done"
+end
+
 puts "Create 10 shops"
 
-10.times do
+12.times do
   Shop.create(name: Faker::Restaurant.name)
 end
 
