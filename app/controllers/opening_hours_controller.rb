ae8e87b9f28f3dd5e7e7be848688cbe6c08fdf32
theobcl <tbancal@free.fr>
2023-01-26 22:21:59 +0100

    add create method to openhour controller

--- app/controllers/opening_hours_controller.rb
+++ app/controllers/opening_hours_controller.rb
@@ -1,2 +1,20 @@
 class OpeningHoursController < ApplicationController
+  before_action :set_shop, only: %i[create]
+
+  def create
+    @opening_hour = OpeningHour.new(opening_hour_params)
+    @opening_hour.shop = @shop
+    @opening_hour.save
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
+  def opening_hour_params
+    params.require(:opening_hour).permit(:day, :is_closed, :start_at, :end_at)
+  end
+
 end
--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -1,16 +1,16 @@
 class OpeningHour < ApplicationRecord
   belongs_to :shop
 
-  extend Enumerize
-  enumerize :day, in: [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
+  # extend Enumerize
+  # enumerize :day, in: [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
 
   validates :shop_id, presence: true
   validates :day, presence: true, inclusion: { in: 0..6 }
-  validates :is_closed, presence: true
+  # validates :is_closed, presence: true
   validates :start_at, presence: true, unless: :is_closed
   validates :end_at, presence: true, unless: :is_closed
 
-  validates_time :start_at, before: :end_at, unless: :is_closed
+  # validates_time :start_at, before: :end_at, unless: :is_closed
 
   validates_uniqueness_of :day, scope: :shop_id, conditions: -> { where(is_closed: true) }, message: "can only have one closed time for a given day"
   validates_uniqueness_of :day, scope: :shop_id, conditions: -> { where(is_closed: false) }, limit: 2, message: "can't have more than two opening hour for the same day"
