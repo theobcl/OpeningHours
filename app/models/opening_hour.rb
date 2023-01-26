ac0958fb92bd5aea931bd1d6283091d3f0345fc9
theobcl <tbancal@free.fr>
2023-01-26 18:28:35 +0100

    add time validation to openinghour model

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -2,11 +2,13 @@ class OpeningHour < ApplicationRecord
   belongs_to :shop
 
   extend Enumerize
-  enumerize :day, in: [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday], default: :monday
+  enumerize :day, in: [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
 
   validates :shop_id, presence: true
   validates :day, presence: true, inclusion: { in: 0..6 }
   validates :is_closed, presence: true
   validates :start_at, presence: :true
   validates :end_at; presence: :true
+
+  validates_time :start_at, before: :end_at
 end
