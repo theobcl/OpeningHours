82470a85c7e456ec20fac2c19af3c9e6443696ea
theobcl <tbancal@free.fr>
2023-01-26 19:03:29 +0100

    improve openinghour model validation

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -7,8 +7,11 @@ class OpeningHour < ApplicationRecord
   validates :shop_id, presence: true
   validates :day, presence: true, inclusion: { in: 0..6 }
   validates :is_closed, presence: true
-  validates :start_at, presence: :true
-  validates :end_at; presence: :true
+  validates :start_at, presence: :true, unless: :is_closed
+  validates :end_at; presence: :true, unless: :is_closed
 
-  validates_time :start_at, before: :end_at
+  validates_time :start_at, before: :end_at, unless: :is_closed
+
+  validates_uniqueness_of :day, scope: :shop_id, conditions: -> { where(is_closed: true) }, message: "can only have one closed time for a given day"
+  validates_uniqueness_of :day, scope: :shop_id, conditions: -> { where(is_closed: false) }, limit: 2, message: "can't have more than two opening hour for the same day"
 end
