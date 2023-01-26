31c8e47f4563ec8dc9d4b60f1f3ec20776802904
theobcl <tbancal@free.fr>
2023-01-26 18:02:12 +0100

    add enumerize gem + use it on day attribute

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -1,3 +1,8 @@
 class OpeningHour < ApplicationRecord
+  extend Enumerize
+
   belongs_to :shop
+
+  enumerize :day, in: [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday], default: :monday
+
 end
