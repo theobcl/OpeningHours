1b271aa9b8ab4a5333bf779a95d3c7037a658bf4
theobcl <tbancal@free.fr>
2023-01-27 02:29:28 +0100

    Improve seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,21 @@ end
 
 puts "Create 10 shops"
 
-1.times do
-  shop = Shop.create(name: Faker::Restaurant.name)
-  OpeningHour.create(shop: shop, day: :monday, is_closed: false, start_at: "09:00", end_at: "12:00")
-  OpeningHour.create(shop: shop, day: :monday, is_closed: true, start_at: "09:00", end_at: "12:00")
-  OpeningHour.create(shop: shop, day: :monday, is_closed: false, start_at: "11:00", end_at: "19:00")
-  OpeningHour.create(shop: shop, day: :monday, is_closed: false, start_at: "13:00", end_at: "19:00")
+days = [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
+
+12.times do
+  shop = Shop.create(name: Faker::Company.name)
+
+  days.each do |day|
+    if rand(0..2) == 0
+      shop.opening_hours.create(day: day, is_closed: true)
+    elsif rand(0..2) == 1
+      shop.opening_hours.create(day: day, start_at: "9:00", end_at: "13:00")
+      shop.opening_hours.create(day: day, start_at: "15:00", end_at: "19:00")
+    else
+      shop.opening_hours.create(day: day, start_at: "9:00", end_at: "19:00")
+    end
+  end
 end
 
 puts "Shops creation done"
