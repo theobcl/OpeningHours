8c9fd34b37c008d9c3737ba1d91c6a8357d74d6c
theobcl <tbancal@free.fr>
2023-01-27 18:04:23 +0100

    Move shop model methods in shops decorator

--- app/decorators/shop_decorator.rb
+++ app/decorators/shop_decorator.rb
@@ -1,3 +1,15 @@
 class ShopDecorator < ApplicationDecorator
   decorates_associations :opening_hours
+
+  def sort_week_days
+    week_days = object.opening_hours.day.values
+    index     = week_days.index(Date.current.strftime("%A").downcase)
+    week_days.rotate(index)
+  end
+
+  def sorted_opening_hours_per_day
+    weekday_order = sort_week_days()
+    grouped_days  = opening_hours.group_by(&:day)
+    grouped_days.sort_by { |key, _value| weekday_order.index(key) }.to_h
+  end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,16 +4,4 @@ class Shop < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   # TO DO : make sure there at least an instance for each day of the week to validate a shop instance
-
-  def sort_week_days()
-    week_days = opening_hours.day.values
-    index = week_days.index(Date.current.strftime("%A").downcase)
-    return week_days.rotate(index)
-  end
-
-  def sorted_opening_hours_per_day
-    weekday_order = sort_week_days()
-    grouped_days = opening_hours.group_by(&:day)
-    sorted_grouped_days = grouped_days.sort_by { |key, _value| weekday_order.index(key) }.to_h
-  end
 end
