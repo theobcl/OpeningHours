5eb62cafee5723bcbc35403ff4f041dc67352bc3
theobcl <tbancal@free.fr>
2023-01-27 10:56:41 +0100

    add instance method in Shop model to display opening hours

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,4 +4,20 @@ class Shop < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   # TO DO : make sure there at least an instance for each day of the week to validate a shop instance
+
+  def week_days()
+    self.opening_hours.day.values
+  end
+
+  def sort_week_days()
+    week_days = week_days()
+    index = week_days.index(Date.current.strftime("%A").downcase)
+    return week_days.rotate(index)
+  end
+
+  def sorted_opening_hours_per_day
+    weekday_order = sort_week_days()
+    grouped_days = self.opening_hours.group_by(&:day)
+    sorted_grouped_days = grouped_days.sort_by { |key, _value| weekday_order.index(key) }.to_h
+  end
 end
